53960f65ee2ad198bd7cb41d300dff95a3d5271c
WarwickSeven <WarwickSeven@gmail.com>
2022-02-18 22:33:19 +0300

    Initial Commit

---
+++ HomeWork 3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HomeWork 3
+//
+//  Created by Александр Тихонов on 18.02.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
